8a2e9737bb79873287eef2747b655c356d7f81d8
Moin Iqbal <moiniqbal205@gmail.com>
2021-02-27 01:11:19 +1100

    1st commit: Vue MemeBook

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-dark bg-dark">
+      <h2>Meme<span>Book</span></h2>
+    </nav>    
+    <div>
+      <b-carousel
+        id="carousel-1"
+        :interval="4000"
+        controls
+        indicators
+        background="#2c3e50"
+        style="text-shadow: 1px 1px 2px #333;"
+      >
+        <!-- Text slides with image -->
+        <b-carousel-slide
+          class="slider"
+          v-for="(meme, index) in memes"
+          :key="index"
+          :caption="meme.name"
+          :img-src="meme.url"
+        ></b-carousel-slide>
+      </b-carousel>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      memes: []
+    }
+  },
+  mounted () {
+    axios.get("https://api.imgflip.com/get_memes")
+    .then(response => {
+        this.memes = response.data.data.memes;
+      });
   }
 }
 </script>
 
 <style>
+
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+h2 {
+  padding: 15px;
+  width: 100%;
+  color: #ccc;
+}
+
+h2 span {
+  color: greenyellow;
+}
+
+.slider {
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: 100% 100%;
+  width: 100%;
+  height: auto;
 }
 </style>
